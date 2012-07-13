37b218426f4f18cf2d0eca5aac60a22f6f289099
Matthias Gugel <gugel.matthias@gmail.com>
2012-07-13 05:55:52 +0200

    new repositories maybe not needed, but easier to use

---
+++ Classes/Domain/Repository/LabRepository.php
@@ -0,0 +1,17 @@
+<?php
+namespace HSE\Labor\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "HSE.Labor".                  *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A repository for Labs
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class LabRepository extends \TYPO3\FLOW3\Persistence\Repository {
+}
---
+++ Classes/Domain/Repository/StudentExerciseRepository.php
@@ -0,0 +1,17 @@
+<?php
+namespace HSE\Labor\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "HSE.Labor".                  *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A repository for StudentExercises
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class StudentExerciseRepository extends \TYPO3\FLOW3\Persistence\Repository {
+}
